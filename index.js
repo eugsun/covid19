2fd597f3e19092d70f2ebfff03b604a804bbd9fa
Eugene Sun <eug.sun@gmail.com>
2020-04-11 13:52:00 -0400

    Add snapshot panel

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function getRandomColorRgb() {
 //
 // APP
 //
+let usCurrentApi = 'https://covidtracking.com/api/v1/us/current.json';
 let api = 'https://covidtracking.com/api/states/daily';
 
 let chart = Vue.component('line-chart', {
@@ -31,6 +32,7 @@ let chart = Vue.component('line-chart', {
     }
 });
 
+
 let app = new Vue({
     el: '#app-chart',
     name: 'COVID-19 USA Growth Factor',
@@ -42,11 +44,22 @@ let app = new Vue({
         chartData: null,
         options: null,
         movingAvgDays: 5,
+        lookbackDays: 30,
         apiResult: null,
+        labels: null,
+        // Snapshot
+        date: '-',
+        usPopulation: 330579861,
+        totalTests: '-',
+        totalPositives: '-',
+        totalDeaths: '-',
+        percentTested: '-',
+        percentPositive: '-',
+        percentDead: '-',
     },
     methods: {
         render: function () {
-            this.movingAvgDays = Number(this.movingAvgDays);
+            // this.movingAvgDays = Number(this.movingAvgDays);
             this.loaded = false;
             let labelSet = new Set();
             let datasetMap = new Map();
@@ -57,21 +70,36 @@ let app = new Vue({
                     datasetMap.set(d.state, {
                         label: d.state,
                         data: [],
+                        metadata: new Map(),
                         borderColor: stateColor,
                         backgroundColor: stateColor,
                         hidden: d.state != "NY" && d.state != "MD",
                         fill: false,
                     })
                 }
-                datasetMap.get(d.state).data.push(d.positiveIncrease);
+                // datasetMap.get(d.state).data.push(d.positiveIncrease);
+                let metadata = datasetMap.get(d.state).metadata;
+                metadata.set(d.date, {
+                    positiveIncrease : d.positiveIncrease,
+                    deathIncrease: d.deathIncrease,
+                });
                 // datasetMap.get(d.state).data.push(d.deathIncrease);
             });
-            let labels = Array.from(labelSet);
-            labels.sort();
+            this.labels = Array.from(labelSet);
+            this.labels.sort();
+
+            let labels = this.labels;
 
             let datasets = Array.from(datasetMap.values());
             datasets.forEach(function(dataset) {
-                dataset.data.reverse();
+                // dataset.data.reverse();
+                labels.forEach(function(dateLabel) {
+                    if (dataset.metadata.has(dateLabel)) {
+                        dataset.data.push(dataset.metadata.get(dateLabel).positiveIncrease);
+                    } else {
+                        dataset.data.push(0);
+                    }
+                });
 
                 let originals = [0];
                 let growthFactors = [1];
@@ -97,13 +125,16 @@ let app = new Vue({
                     let sumOriginalNow = originals.slice(i-n+1, i+1)
                                                   .reduce((a, b) => a+b, 0) / n;
                     let val = sumOriginalNow < 0.1 || sumOriginal < 0.1 ? 1 : sumOriginalNow / sumOriginal;
-                    console.log(`for ${i}: ${sumOriginal} => ${sumOriginalNow} Grew by ${val}`);
+                    // console.log(`for ${i}: ${sumOriginal} => ${sumOriginalNow} Grew by ${val}`);
                     avgGrowthFactors.push(val);
                 }
-                dataset.data = avgGrowthFactors;
+                // dataset.data = avgGrowthFactors;
+                dataset.data = avgGrowthFactors.slice(
+                    avgGrowthFactors.length - app.lookbackDays,
+                    avgGrowthFactors.length);
             });
             this.chartData = {
-                labels: labels,
+                labels: labels.slice(labels.length - app.lookbackDays, labels.length),
                 datasets: datasets,
             };
             this.options = {
@@ -149,23 +180,52 @@ let app = new Vue({
                         },
                         ticks: {
                             min: 0,
-                            max: 3,
+                            // max: 3,
                             stepSize: 0.5,
                         }
                     }],
                 },
             };
             this.loaded = true;
-        }
+        },
+        renderSnapshot: function () {
+            this.loaded = false;
+
+            let totalTests = this.usSnapshot.totalTestResults;
+            let totalPositives = this.usSnapshot.positive;
+            let totalDeaths = this.usSnapshot.death;
+            let percentTested = totalTests / this.usPopulation;
+            let percentPositive = totalPositives / totalTests;
+            let percentDead = totalDeaths / totalPositives;
+
+            this.date = this.usSnapshot.lastModified.split('T')[0];
+            this.totalTests = totalTests.toLocaleString();
+            this.totalPositives = totalPositives.toLocaleString();
+            this.totalDeaths = totalDeaths.toLocaleString();
+
+            this.percentTested = percentTested.toLocaleString('en', {style: 'percent'}) + ' of population'
+            this.percentPositive = percentPositive.toLocaleString('en', {style: 'percent'}) + ' of all tests';
+            this.percentDead = percentDead.toLocaleString('en', {style: 'percent'}) + ' of all positives';
+
+            this.loaded = true;
+        },
     },
     mounted () {
+        fetch(usCurrentApi)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.usSnapshot = data[0];
+                app.renderSnapshot();
+            });
         fetch(api)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 this.apiResult = data;
-                console.log(this.apiResult);
+                // console.log(this.apiResult);
                 app.render();
             })
         ;
